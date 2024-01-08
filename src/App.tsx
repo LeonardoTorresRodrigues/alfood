d45168c1c3c579788e2096cc93bc2f813b7201af
Leonardo Torres Rodrigues <leonardo.t.rodri@gmail.com>
2024-01-08 13:20:55 -0300

    feat: add admin page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './paginas/Home';
 import VitrineRestaurantes from './paginas/VitrineRestaurantes';
+import AdministracaoRestaurantes from './paginas/Administracao/Restaurantes/AdministracaoRestaurante';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/restaurantes" element={<VitrineRestaurantes />} />
+      <Route
+        path="/admin/restaurantes"
+        element={<AdministracaoRestaurantes />}
+      />
     </Routes>
   );
 }
