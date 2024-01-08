6dfc2a702788a36926c492a53387999e72238481
Leonardo Torres Rodrigues <leonardo.t.rodri@gmail.com>
2024-01-08 12:38:55 -0300

    fix: add React on the scope

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './paginas/Home';
 import VitrineRestaurantes from './paginas/VitrineRestaurantes';
 
 function App() {
-
   return (
     <Routes>
       <Route path="/" element={<Home />} />
