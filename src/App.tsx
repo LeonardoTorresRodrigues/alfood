4cd262e981c6d23c0c9de5397d2a11d87d673294
Leonardo Torres Rodrigues <leonardo.t.rodri@gmail.com>
2024-01-16 10:56:43 -0300

    feat: add admin edit option

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,10 @@ function App() {
         path="/admin/restaurantes/novo"
         element={<FormularioRestaurante />}
       />
+      <Route
+        path="/admin/restaurantes/:id"
+        element={<FormularioRestaurante />}
+      />
     </Routes>
   );
 }
